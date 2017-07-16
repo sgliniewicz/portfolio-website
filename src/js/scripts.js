6f3efdb8f045773de99818314300396b396013d8
Steven Gliniewicz <sgliniewicz@Stevens-MacBook-Pro.local>
2017-07-16 16:24:10 -0500

    nav updates

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,49 @@
 $(function() {
     console.log( "ready!" );
 
-    $('.btn-floating').click(function(){
-        $(this).addClass('scale-out')
+    var menuIndex = 0
+    var menuBtn = $('.menu-btn');
+    var menuItems = $('.menu-item');
+
+
+    menuBtn.click(function(){
+        if (menuBtn.hasClass('menu-closed')){
+            openMenuWithDelay();
+        } else {
+
+            closeMenuWithDelay();
+        }
     });
+
+    function closeMenuWithDelay(){ 
+        var delay = setInterval( function(){
+            console.log("index: " + menuIndex)
+            if ( menuIndex >= 0 ){
+                $( menuItems[ menuIndex ] ).removeClass( 'scale-in' );
+                menuIndex -= 1;
+            } else{
+                clearInterval( delay );
+            }
+        }, 100 );   
+
+        menuBtn.removeClass("menu-open");
+        menuBtn.addClass("menu-closed");
+    }
+    
+    function openMenuWithDelay(){ 
+        var delay = setInterval( function(){
+            console.log("index: " + menuIndex)
+            if ( menuIndex <  menuItems.length ){
+                $( menuItems[ menuIndex ] ).addClass( 'scale-in' );
+                menuIndex += 1;
+            } else{
+                clearInterval( delay );
+            }
+        }, 100);
+        menuBtn.removeClass("menu-closed");
+        menuBtn.addClass("menu-open");
+    }
+    
+    openMenuWithDelay();
+    
 });
