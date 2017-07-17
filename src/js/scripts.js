f2542dc5f99ef99f9313f9681d7fd9058ecb7173
Gliniewicz <tnw113@784f43a6282d.cof.ds.capitalone.com>
2017-07-17 12:37:50 -0500

    added social media Icons and Scroll functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,20 +3,42 @@ $(function() {
     console.log( "ready!" );
 
     var menuIndex = 0
+    var iScrollPos = 0;
+    var $window = $(window)
     var menuBtn = $('.menu-btn');
     var menuItems = $('.menu-item');
 
+    $window.scroll(function () {
+
+        var iCurScrollPos = $(this).scrollTop();
+        menuItems.toggleClass('scale-in', iCurScrollPos < iScrollPos)
+        
+        if (iCurScrollPos  > iScrollPos) {
+            //down
+            menuBtn.removeClass("menu-open");
+            menuBtn.addClass("menu-closed");
+            menuIndex = -1;
+
+        } else {
+            //up
+            menuBtn.removeClass("menu-closed");
+            menuBtn.addClass("menu-open");
+            menuIndex = 4;
+        }
+        iScrollPos = iCurScrollPos;
+        
+    });
 
     menuBtn.click(function(){
         if (menuBtn.hasClass('menu-closed')){
             openMenuWithDelay();
         } else {
-
             closeMenuWithDelay();
         }
     });
 
     function closeMenuWithDelay(){ 
+
         var delay = setInterval( function(){
             console.log("index: " + menuIndex)
             if ( menuIndex >= 0 ){
@@ -26,12 +48,13 @@ $(function() {
                 clearInterval( delay );
             }
         }, 100 );   
-
+        
         menuBtn.removeClass("menu-open");
         menuBtn.addClass("menu-closed");
     }
     
-    function openMenuWithDelay(){ 
+    function openMenuWithDelay(){
+
         var delay = setInterval( function(){
             console.log("index: " + menuIndex)
             if ( menuIndex <  menuItems.length ){
@@ -41,6 +64,7 @@ $(function() {
                 clearInterval( delay );
             }
         }, 100);
+
         menuBtn.removeClass("menu-closed");
         menuBtn.addClass("menu-open");
     }
