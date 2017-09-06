dae95c665c857a093d29ae29b8f1e35c4b7f9780
Gliniewicz <tnw113@784f43a6282d.cof.ds.capitalone.com>
2017-09-06 10:06:37 -0500

    mobile nav bar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,26 +4,27 @@ $(function() {
 
     var delay;
     var scrolling = false;
-    var menuIndex = 0
+    var menuIndex = 0;
     var iScrollPos = 0;
-    var $window = $(window)
-    var menuItems = $('.menu-item');
+    var $window = $(window);
+    var menuItems = $('.nav-item');
+    var navItemsMobile = $('.nav-item-mobile');
     var mediaItems = $('.social-media-item');
     var fullHeight = $(".full-height");
 
     function landingPageFlow() {
         //title animation
-        $('#title').addClass('fadeInDown')
-        $('#subTitle').addClass('fadeInDown')
-        $('#me').addClass('fadeInDown')
+        $('#title').addClass('fadeInDown');
+        $('#subTitle').addClass('fadeInDown');
+        $('#me').addClass('fadeInDown');
         
         setTimeout( function(){
-            $('.social-media-item').addClass('scale-in')
+            $('.social-media-item').addClass('scale-in');
         }, 500)
 
         //chevron
         setTimeout( function(){
-            $('#scroll-down-icon').addClass('scale-in')
+            $('#scroll-down-icon').addClass('scale-in');
         }, 1000)
     }
 
@@ -35,6 +36,11 @@ $(function() {
             $("#quote2").addClass('fadeIn');
           } },
     ]
+
+    $('#mobile-nav-btn').on('click', function(){
+        navItemsMobile.toggleClass('scale-in');
+    
+    });
 
     landingPageFlow();
     $('.parallax').parallax();
